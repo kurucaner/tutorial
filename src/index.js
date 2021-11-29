fc07b12a620f98e6c847697fe366148ad74265a4
Caner <kurucaner@gmail.com>
2021-11-29 18:25:07 -0500

    JSX Javascript

--- src/index.js
+++ src/index.js
@@ -8,35 +8,26 @@ function BookList() {
   return (
     <section class="booklist">
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
 
 const Book = () => {
+  const title = "The Book of Trees";
+  const author = "Bill Gates";
   return (
-    <article>
-      <Image></Image>
-      <Title />
-      <Author />
+    <article className="book">
+      <img
+        src="https://images.unsplash.com/photo-1497633762265-9d179a990aa6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"
+        alt="Image of a book"
+        width="300"
+        height="300"
+      />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {/* <p>{let x = 6}</p> */}
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src="https://images.unsplash.com/photo-1497633762265-9d179a990aa6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"
-    alt="Image of a book"
-    width="300"
-    height="300"
-  />
-);
-
-const Title = () => <h1>Inception</h1>;
-const Author = () => <h2>Christopher Nolan</h2>;
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
