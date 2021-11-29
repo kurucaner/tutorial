941c0de13c32c8148edc6cc7a498e55d60f5ac5c
Caner <kurucaner@gmail.com>
2021-11-29 18:44:59 -0500

    added basic props

--- src/index.js
+++ src/index.js
@@ -6,29 +6,45 @@ import "./index.css";
 
 //Setup vars
 
+const firstBook = {
+  title: "The Lord of the Rings",
+  author: "J.R.R. Tolkien",
+  img: "https://m.media-amazon.com/images/I/61uFYcoYRRL.jpg",
+};
+
+const secondBook = {
+  title: "The Hobbit",
+  author: "J.R.R. Tolkien",
+  img: "https://m.media-amazon.com/images/I/51lKk3HUNlS.jpg",
+};
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book job="developer" />
-      <Book title="random title" />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
 const Book = (props) => {
-  console.log(props);
   const title = "The Book of Trees";
   const author = "Bill Gates";
   const img =
     "https://images.unsplash.com/photo-1497633762265-9d179a990aa6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80";
   return (
     <article className="book">
-      <img src={img} alt="Image of a book" width="300" height="300" />
-      <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
-      {console.log(props)}
-      <p>{props.job}</p>
-      <p>{props.title}</p>
+      <img src={props.img} alt="Image of a book" width="300" height="300" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
